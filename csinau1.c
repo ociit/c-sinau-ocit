acb406a8e80dd24e276917bf90b61f137f6aed97
ociit <r2ochidridho828@gmail.com>
2023-08-07 11:33:13 +0700

    pp

--- csinau1.c
+++ csinau1.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    //masukkan input angka bulat, character, dan float (pecahan)
-    int angka = 10;
-    char huruf = 'A';
-    float pecahan = 5.3;
 
-    //panggil melalui printf 
-    printf ("Angka : %d\n", angka);
-    printf ("Huruf : %c\n", huruf);
-    printf ("Pecahan : %.2f\n", pecahan);
-    //%d untk var. bil. bulat //var. --> variable
-    //%c untk var. character 
-    //%f untk var. bil. pecahan, angka didepan penanda maks bil dibelakang koma
+    int x;          //declaration
+    x = 123;        //initialization
+    int y = 321;    //declaration + initialization
+
+    //masukkan input angka bulat, character, dan float (pecahan)
+    int angka = 10;             //integer 
+    char huruf = 'A';           //single char
+    float pecahan = 5.3;      //floating / decimal
+    char more [] = "yo";    //array of char
+
+    //panggil melalui printf
+    
+    printf ("hasil declar dan inis x : %d\n", x);
+    printf ("hasil declar dan inis y : %d\n", y);
+
+    printf ("Angka : %d\n", angka);         //%d untuk tipe data bil. bulat
+    printf ("Huruf : %c\n", huruf);         //%c untuk tipe data 1 char saja 
+    printf ("Pecahan : %.2f\n", pecahan);   //%f untuk tipe data desimal; titik dan angka diantaranya adalah penanda jumlah angka dibelakang koma
+    printf ("kata : %s\n", more);           //%s untuk tipe data yang punya char lebih dari 1 huruf 
     // \n untk bari space //
 
     //input ma printfnya bisa disingkat code nya biar hemat kyk gini ngab
@@ -23,4 +31,6 @@ int main() {
     //cara lain pake printf 
     printf("\n\nperingkat medali emas= %d, \nperingkat medali perunggu= %d, \nperingkat yang kesiangan= %d", 1,2,3);
     //telen aja dlu klo gphm nnt belajar lgi >_<
+
+    return 0;
 }
--- tempCodeRunnerFile.c
+++
@@ -1,3 +0,0 @@
- // for (int i=1; i<=20; i++) {             //menghitung 1-20 secara 1 per satu
-    //     printf ("%d\n", i);
-    // }
